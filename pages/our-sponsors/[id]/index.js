fbf2fc51c5076a46187e42f295e43522a7e73950
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-12 07:00:28 +0300

    Link navigation to dynamic routes

--- pages/our-sponsors/[id]/index.js
+++ pages/our-sponsors/[id]/index.js
@@ -1,6 +1,10 @@
-const AllSponsorProjects = () => {
+import { useRouter } from 'next/router';
+
+const SponsorInfo = () => {
+const router = useRouter();
+
 return <div>
-    <h1>All Sponsor Projects</h1>
+    <h1>{`Sponsor ${router.query.id}`}</h1>
 </div>;
 }
-export default AllSponsorProjects;
+export default SponsorInfo;
--- pages/our-sponsors/index.js
+++ pages/our-sponsors/index.js
@@ -1,6 +1,17 @@
+import Link from 'next/link';
+
 const OurSponsors = () => {
+    const allSponsors = [
+        {id:1, name: 'Deez Nuts'},
+        {id:2, name: 'Likon Deez Nuts'}
+    ]
 return <div>
     <h1>Our Sponsors</h1>
+    <ul>
+        {allSponsors.map((item,index)=><li key={index}>
+            <Link href={`/our-sponsors/${item.id}`}>{item.name}</Link>
+        </li>)}
+    </ul>
 </div>;
 }
 export default OurSponsors;
