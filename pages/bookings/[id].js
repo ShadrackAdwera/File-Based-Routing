c919cdd7baf14321b703fc4ddcbbe59930afc02e
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-12 06:06:57 +0300

    extract dynamic data from url path

--- pages/bookings/[id].js
+++ pages/bookings/[id].js
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router';
+
 const BookingDetails = () => {
+    const router = useRouter();
+
     return <div>
-        <h1>Booking Details Page.</h1>
+        <h1>{`Booking Details for item with ID: ${router.query.id}`}</h1>
     </div>;
     }
     export default BookingDetails;
