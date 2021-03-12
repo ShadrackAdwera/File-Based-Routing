12b170f670dce467ac24044b1530836bd1c80f39
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-12 06:50:23 +0300

    Navigation using the link component to preserve app wide state

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
+import Link from 'next/link';
+
 const HomePage = () => {
     return <div>
-        <h1>This is the HomePage.</h1>
+        <h1>This is the HomePage</h1>
+        <ul>
+            <li>
+                <Link href='/about'>About Us</Link>
+            </li>
+            <li>
+                <Link href='/bookings'>Bookings</Link>
+            </li>
+            <li>
+                <Link href='/our-sponsors'>Our Sponsors</Link>
+            </li>
+        </ul>
     </div>;
 }
 
