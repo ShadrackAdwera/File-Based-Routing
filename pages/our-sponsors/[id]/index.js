4bbe6d8afef3898b1838e644d8190e9326854aa6
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-16 05:49:54 +0300

    Navigate programatically

--- pages/our-sponsors/[id]/index.js
+++ pages/our-sponsors/[id]/index.js
@@ -5,6 +5,7 @@ const router = useRouter();
 
 return <div>
     <h1>{`Sponsor ${router.query.id}`}</h1>
+    <button onClick={()=>router.push('/our-sponsors/1/projects/1')}></button>
 </div>;
 }
 export default SponsorInfo;
