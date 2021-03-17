044f8b88a3b0aeb2e52d65db5d4bd9baba550d6e
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-17 06:58:35 +0300

    fix h2 styling issue

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,12 +1,27 @@
+import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 import { getEventById } from '../../dummy-parties';
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 const PartyDetails = () => {
     const router = useRouter();
     const foundEvent = getEventById(router.query.id);
-    return <div>
-        {foundEvent.description}
-    </div>;
+    if(!foundEvent) {
+        return <h5>No event found</h5>
+    }
+    return <Fragment>
+        <EventSummary title={foundEvent.title}/>
+        <EventLogistics date={foundEvent.date} 
+        address={foundEvent.location} 
+        image={foundEvent.image} 
+        imageAlt={foundEvent.title}/>
+        <EventContent>
+            <p>{foundEvent.description}</p>
+        </EventContent>
+    </Fragment>;
 }
 
 export default PartyDetails;
