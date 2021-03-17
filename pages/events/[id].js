30ba07895449766ab45eeaf17aaeeef2eb00e969
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-17 06:32:52 +0300

    add event details components

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-parties';
 
 const PartyDetails = () => {
     const router = useRouter();
-    
+    const foundEvent = getEventById(router.query.id);
     return <div>
-        <p>{`Party Details: ${router.query.id}`}</p>
+        {foundEvent.description}
     </div>;
 }
 
