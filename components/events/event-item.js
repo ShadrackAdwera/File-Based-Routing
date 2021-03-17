f54c57042434c91bf2d3decb9db32846c2b6667e
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-17 06:25:02 +0300

    add heroicons to events card

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
-import Link from 'next/link';
 import Button from '../ui/Button';
 import styles from './event-item.module.css';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 const EventItem = props => {
     return <li className={styles.item}>
@@ -9,6 +11,7 @@ const EventItem = props => {
             <div className={styles.summary}>
             <h2>{props.title}</h2>
             <div className={styles.date}>
+                <DateIcon />
                 <time>{new Date(props.date).toLocaleDateString('en-US', {
                     day: 'numeric',
                     month:'long',
@@ -16,11 +19,17 @@ const EventItem = props => {
                 })}</time>
             </div>
             <div className={styles.address}>
+                <AddressIcon />
                 <address>{props.location.replace(', ','\n')}</address>
             </div>
             </div>
             <div className={styles.actions}>
-            <Button link={`/events/${props.id}`}>Explore Event</Button>      
+            <Button link={`/events/${props.id}`}>
+                <span>Explore Event</span>
+                <span className={styles.icon}>
+                    <ArrowRightIcon />
+                </span>
+                </Button>      
             </div>
         </div>
     </li>
