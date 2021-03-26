80a1a17881f94bf321e4e3dc206681cd5ffce830
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-26 04:03:18 +0300

    add meta tag to all pages via the app.js file

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,8 +1,13 @@
 import { Fragment } from "react";
+import Head from 'next/head';
+
 import MainHeader from './main-header';
 
 const Layout = props => {
 return <Fragment>
+    <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
     <MainHeader />
     <main>
         {props.children}
