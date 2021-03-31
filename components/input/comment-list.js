0e74a8a358178b841f722a78657b90a4c966fbc0
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-31 05:09:16 +0300

    add a comment list component

---
+++ components/input/comment-list.js
@@ -0,0 +1,23 @@
+import styles from './comment-list.module.css';
+
+function CommentList() {
+  return (
+    <ul className={styles.comments}>
+      {/* Render list of comments - fetched from API */}
+      <li>
+        <p>This is my comment Cabron!!</p>
+        <div>
+          By <address>Deez Nuts</address>
+        </div>
+      </li>
+      <li>
+        <p>Another comment yo!</p>
+        <div>
+          By <address>Likon Deez Nuts</address>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default CommentList;
