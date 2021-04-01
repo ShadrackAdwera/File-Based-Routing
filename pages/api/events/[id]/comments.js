0c153dca628e5041bfe5625922df57c89610fcc5
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-01 04:34:21 +0300

    post comments to mongo db

--- pages/api/events/[id]/comments.js
+++ pages/api/events/[id]/comments.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
-const commentsHandler = (req,res) => {
+const commentsHandler = async(req,res) => {
     if(req.method==='POST') {
         const eventId = req.query.id;
         const { email, name, text } = req.body;
-
-        const filePath = path.join(process.cwd(),'data','comments.json');
-        const stringifiedData = fs.readFileSync(filePath);
-        const data = JSON.parse(stringifiedData);
-        data.unshift({id: Date.now(), name: name, email: email, comment: text, eventId: eventId});
-        fs.writeFileSync(filePath, JSON.stringify(data));
-        return res.status(201).json({message: 'Your comment has been posted!'});
+        // const filePath = path.join(process.cwd(),'data','comments.json');
+        // const stringifiedData = fs.readFileSync(filePath);
+        // const data = JSON.parse(stringifiedData);
+        // data.unshift({id: Date.now(), name: name, email: email, comment: text, eventId: eventId});
+        // fs.writeFileSync(filePath, JSON.stringify(data));
+        try {
+            const client = await MongoClient.connect('mongodb+srv://next_party_user:kawz8hoI89Whafwp@cluster0.dska4.mongodb.net/next-party?retryWrites=true&w=majority')
+        const dbConnected = client.db();
+        await dbConnected.collection('comments').insertOne({name,email,comment:text, eventId});
+        res.status(201).json({message: 'Your comment has been posted!'});
+        client.close();
+        } catch (error) {
+        console.log(error);    
+        }
     } else {
         const eventId = req.query.id;
         const filePath = path.join(process.cwd(),'data','comments.json');
