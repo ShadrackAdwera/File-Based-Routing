a78610c98dabdbaeff83f2f1ea62956c851987c2
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-01 05:06:37 +0300

    add error handling in the newsletter route

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -2,9 +2,19 @@
 // import path from 'path';
 import { MongoClient } from 'mongodb';
 
+const connectToDB = async() => {
+    const client = await MongoClient.connect('mongodb+srv://next_party_user:kawz8hoI89Whafwp@cluster0.dska4.mongodb.net/next-party?retryWrites=true&w=majority')
+    return client;
+}
+
+const insertDoc = async(client, doc) => {
+    const dbCollection = client.db();
+    await dbCollection.collection('emails').insertOne(doc);
+}
 
 const NewsletterHandler = async(req,res) => {
     if(req.method==='POST') {
+        let client;
         const emailAddress = req.body.email;
         // const filePath = path.join(process.cwd(),'data','emails.json');
         // const stringifiedData = fs.readFileSync(filePath);
@@ -12,15 +22,19 @@ const NewsletterHandler = async(req,res) => {
         // data.unshift({id: Date.now(), email: emailAddress});
         // fs.writeFileSync(filePath,JSON.stringify(data));
 
+        try {
+            client = await connectToDB();
+        } catch (error) {
+            return res.status(500).json({message: 'Unable to connect to the DB'}); 
+        }
+
        try {
-        const client = await MongoClient.connect('mongodb+srv://next_party_user:kawz8hoI89Whafwp@cluster0.dska4.mongodb.net/next-party?retryWrites=true&w=majority')
-        const dbCollection = client.db();
-        await dbCollection.collection('emails').insertOne({email: emailAddress});
+        await insertDoc(client, {email: emailAddress});
         client.close();
+        return res.status(201).json({message: 'Email Address registered!'});
        } catch (error) {
-           console.log(error);
+           return res.status(500).json({message: 'Failed to save the data'});     
        } 
-      return res.status(201).json({message: 'Email Address registered!'});
     }
 }
 
