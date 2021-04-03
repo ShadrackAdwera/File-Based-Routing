32d1331950cb2b778b3a1ebc5775c6ff51795b9a
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-03 08:08:59 +0300

    add a notification context component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import '../styles/globals.css'
 import Layout from '../components/layout/layout';
+import '../styles/globals.css'
+import Notification from '../components/notification/notification';
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
     <Component {...pageProps} />
+    <Notification title='Hello World' message='Hello Muthafaka!' status='pending'/>
   </Layout>
 }
 
---
+++ store/notification-context.js
@@ -0,0 +1,15 @@
+import { createContext } from 'react';
+
+const NotificationContext = createContext({
+    notification: null,
+    showNotification: () => {},
+    hideNotification: () => {}
+});
+
+export const NotificationContextProvider = props => {
+    return <NotificationContext.Provider>
+        {props.children}
+    </NotificationContext.Provider>
+}
+
+export default NotificationContext
